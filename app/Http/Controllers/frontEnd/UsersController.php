11fd7ccbd6adf3df1046baca82b62d96a3128c87
gutropolis <gutropolis@gmail.com>
2018-06-15 17:22:42 +0530

    add

---
+++ app/Http/Controllers/frontEnd/UsersController.php
@@ -0,0 +1,501 @@
+<?php
+
+namespace App\Http\Controllers\frontEnd;
+use App\Http\Controllers\FrontendHomeController;
+use App\Http\Controllers\WebmasterSettings;
+use App\Http\Requests;
+use App\Permissions;
+use App\Country;
+use App\User;
+use App\WebmasterSection;
+use App\WebmasterSetting;
+use App\Setting;
+use App\Menu;
+use App\Topic;
+use App\Banner;
+use Auth;
+use File;
+use Illuminate\Config;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+
+class UsersController extends FrontendHomeController
+{
+
+    private $uploadPath = "uploads/users/";
+
+    // Define Default Variables
+
+  
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        // General for all pages
+        $GeneralWebmasterSections = WebmasterSection::where('status', '=', '1')->orderby('row_no', 'asc')->get();
+        // General END
+
+        if (@Auth::user()->permissionsGroup->view_status) {
+            $Users = User::where('created_by', '=', Auth::user()->id)->orwhere('id', '=', Auth::user()->id)->orderby('id',
+                'asc')->paginate(env('BACKEND_PAGINATION'));
+            $Permissions = Permissions::where('created_by', '=', Auth::user()->id)->orderby('id', 'asc')->get();
+        } else {
+            $Users = User::orderby('id', 'asc')->paginate(env('BACKEND_PAGINATION'));
+            $Permissions = Permissions::orderby('id', 'asc')->get();
+        }
+        return view("backEnd.users", compact("Users", "Permissions", "GeneralWebmasterSections"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        // General for all pages
+        $GeneralWebmasterSections = WebmasterSection::where('status', '=', '1')->orderby('row_no', 'asc')->get();
+        // General END
+        $Permissions = Permissions::orderby('id', 'asc')->get();
+
+        return view("backEnd.users.create", compact("GeneralWebmasterSections", "Permissions"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request, [
+            'photo' => 'mimes:png,jpeg,jpg,gif|max:3000',
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'permissions_id' => 'required'
+        ]);
+
+
+        // Start of Upload Files
+        $formFileName = "photo";
+        $fileFinalName_ar = "";
+        if ($request->$formFileName != "") {
+            $fileFinalName_ar = time() . rand(1111,
+                    9999) . '.' . $request->file($formFileName)->getClientOriginalExtension();
+            $path = base_path() . "/public/" . $this->getUploadPath();
+            $request->file($formFileName)->move($path, $fileFinalName_ar);
+        }
+        // End of Upload Files
+
+        $User = new User;
+        $User->name = $request->name;
+        $User->email = $request->email;
+        $User->password = bcrypt($request->password);
+        $User->permissions_id = $request->permissions_id;
+        $User->photo = $fileFinalName_ar;
+        $User->connect_email = $request->connect_email;
+        $User->connect_password = $request->connect_password;
+        $User->status = 1;
+        $User->created_by = Auth::user()->id;
+        $User->save();
+
+        return redirect()->action('UsersController@index')->with('doneMessage', trans('backLang.addDone'));
+    }
+
+    public function getUploadPath()
+    {
+        return $this->uploadPath;
+    }
+
+    public function setUploadPath($uploadPath)
+    {
+        $this->uploadPath = Config::get('app.APP_URL') . $uploadPath;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit_profile($id,$lang="")
+    {
+		
+		$user=User::find($id);
+ if ($lang != "") {
+            // Set Language
+            App::setLocale($lang);
+            \Session::put('locale', $lang);
+        }
+        // General Webmaster Settings
+        $WebmasterSettings = WebmasterSetting::find(1);
+
+        // General for all pages
+        $WebsiteSettings = Setting::find(1);
+        $HeaderMenuLinks = Menu::where('father_id', $WebmasterSettings->header_menu_id)->where('status',
+            1)->orderby('row_no',
+            'asc')->get();
+        $FooterMenuLinks = Menu::where('father_id', $WebmasterSettings->footer_menu_id)->where('status',
+            1)->orderby('row_no',
+            'asc')->get();
+        $FooterMenuLinks_father = Menu::find($WebmasterSettings->footer_menu_id);
+        $FooterMenuLinks_name_ar = "";
+        $FooterMenuLinks_name_en = "";
+        if (count($FooterMenuLinks_father) > 0) {
+            $FooterMenuLinks_name_ar = $FooterMenuLinks_father->title_ar;
+            $FooterMenuLinks_name_en = $FooterMenuLinks_father->title_en;
+        }
+
+        // Home topics
+        $HomeTopics = Topic::where([['status', 1], ['webmaster_id', $WebmasterSettings->home_content1_section_id], ['expire_date', '>=', date("Y-m-d")], ['expire_date', '<>', null]])->orwhere([['status', 1], ['webmaster_id', $WebmasterSettings->home_content1_section_id], ['expire_date', null]])->orderby('row_no', 'asc')->limit(3)->get();
+        // Home photos
+        $HomePhotos = Topic::where([['status', 1], ['webmaster_id', $WebmasterSettings->home_content2_section_id], ['expire_date', '>=', date("Y-m-d")], ['expire_date', '<>', null]])->orwhere([['status', 1], ['webmaster_id', $WebmasterSettings->home_content2_section_id], ['expire_date', null]])->orderby('row_no', 'asc')->limit(6)->get();
+// Home Partners
+        $HomePartners = Topic::where([['status', 1], ['webmaster_id', $WebmasterSettings->home_content3_section_id], ['expire_date', '>=', date("Y-m-d")], ['expire_date', '<>', null]])->orwhere([['status', 1], ['webmaster_id', $WebmasterSettings->home_content3_section_id], ['expire_date', null]])->orderby('row_no', 'asc')->get();
+
+        // Get Latest News
+        $LatestNews = Topic::where([['status', 1], ['webmaster_id', $WebmasterSettings->latest_news_section_id], ['expire_date', '>=', date("Y-m-d")], ['expire_date', '<>', null]])->orwhere([['status', 1], ['webmaster_id', $WebmasterSettings->latest_news_section_id], ['expire_date', null]])->orderby('row_no', 'asc')->limit(3)->get();
+
+        // Get Home page slider banners
+        $SliderBanners = Banner::where('section_id', $WebmasterSettings->home_banners_section_id)->where('status',
+            1)->orderby('row_no', 'asc')->get();
+
+        // Get Home page Test banners
+        $TextBanners = Banner::where('section_id', $WebmasterSettings->home_text_banners_section_id)->where('status',
+            1)->orderby('row_no', 'asc')->get();
+
+        $site_desc_var = "site_desc_" . trans('backLang.boxCode');
+        $site_keywords_var = "site_keywords_" . trans('backLang.boxCode');
+
+        $PageTitle = ""; // will show default site Title
+        $PageDescription = $WebsiteSettings->$site_desc_var;
+        $PageKeywords = $WebsiteSettings->$site_keywords_var;
+		$countries=Country::pluck('title_en');
+        return view("frontEnd.users.edit_profile",
+            compact("WebsiteSettings",
+			"user",
+                "WebmasterSettings",
+				"countries",
+                "HeaderMenuLinks",
+                "FooterMenuLinks",
+                "SliderBanners",
+                "TextBanners",
+                "FooterMenuLinks_name_ar",
+                "FooterMenuLinks_name_en",
+                "PageTitle",
+                "PageDescription",
+                "PageKeywords",
+                "PageTitle",
+                "PageDescription",
+                "PageKeywords",
+                "HomeTopics",
+                "HomePhotos",
+                "HomePartners",
+                "LatestNews"));
+
+		
+		//return $user;
+       //return view("frontEnd.users.edit_profile",compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $user = User::find($id);
+		
+        if (count($user) > 0) {
+
+
+            if ($request->email != $user->email) {
+                $this->validate($request, [
+                    'email' => 'required|email|unique:users',
+                ]);
+            }
+            // Start of Upload Files
+            $formFileName = "photo";
+            $fileFinalName_ar = "";
+            if ($request->$formFileName != "") {
+                $fileFinalName_ar = time() . rand(1111,
+                        9999) . '.' . $request->file($formFileName)->getClientOriginalExtension();
+                $path = base_path() . "/public/" . $this->getUploadPath();
+                $request->file($formFileName)->move($path, $fileFinalName_ar);
+            }
+            // End of Upload Files
+
+            //if ($id != 1) {
+            $user->first_name = $request->first_name;
+			$user->last_name = $request->last_name;
+            $user->email = $request->email;
+			$user->address = $request->address;
+			$user->phone = $request->PhoneNo;
+			$user->post_code = $request->post_code;
+			$user->gender = $request->gender;
+			$user->dob = $request->dob;
+            if ($request->password != "") {
+                $user->password = bcrypt($request->password);
+            }
+            
+                     if ($request->photo_delete == 1) {
+                // Delete a User file
+                if ($user->photo != "") {
+                    File::delete($this->getUploadPath() . $user->photo);
+                }
+
+                $user->photo = "";
+            }
+            if ($fileFinalName_ar != "") {
+                // Delete a User file
+                if ($user->photo != "") {
+                    File::delete($this->getUploadPath() . $user->photo);
+                }
+
+                $user->photo = $fileFinalName_ar;
+            }
+
+          
+         
+            $user->updated_by = $request->first_name;
+            $user->save();
+          return Redirect::route("Home")->with('success', trans('Profile Updated'));
+        } else {
+            return redirect()->action('UsersController@edit_profile');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        if (@Auth::user()->permissionsGroup->view_status) {
+            $User = User::where('created_by', '=', Auth::user()->id)->find($id);
+        } else {
+            $User = User::find($id);
+        }
+        if (count($User) > 0 && $id != 1) {
+            // Delete a User photo
+            if ($User->photo != "") {
+                File::delete($this->getUploadPath() . $User->photo);
+            }
+
+            $User->delete();
+            return redirect()->action('UsersController@index')->with('doneMessage', trans('backLang.deleteDone'));
+        } else {
+            return redirect()->action('UsersController@index');
+        }
+    }
+
+
+    /**
+     * Update all selected resources in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  buttonNames , array $ids[]
+     * @return \Illuminate\Http\Response
+     */
+    public function updateAll(Request $request)
+    {
+        //
+        if ($request->action == "activate") {
+            User::wherein('id', $request->ids)
+                ->update(['status' => 1]);
+
+        } elseif ($request->action == "block") {
+            User::wherein('id', $request->ids)->where('id', '!=', 1)
+                ->update(['status' => 0]);
+
+        } elseif ($request->action == "delete") {
+            // Delete User photo
+            $Users = User::wherein('id', $request->ids)->where('id', '!=', 1)->get();
+            foreach ($Users as $User) {
+                if ($User->photo != "") {
+                    File::delete($this->getUploadPath() . $User->photo);
+                }
+            }
+
+            User::wherein('id', $request->ids)->where('id', "!=", 1)
+                ->delete();
+
+        }
+        return redirect()->action('UsersController@index')->with('doneMessage', trans('backLang.saveDone'));
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function permissions_create()
+    {
+        //
+        // General for all pages
+        $GeneralWebmasterSections = WebmasterSection::where('status', '=', '1')->orderby('row_no', 'asc')->get();
+        // General END
+
+        return view("backEnd.users.permissions.create", compact("GeneralWebmasterSections"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function permissions_store(Request $request)
+    {
+        //
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+
+        $data_sections_values = "";
+        if (@$request->data_sections != "") {
+            foreach ($request->data_sections as $key => $val) {
+                $data_sections_values = $val . "," . $data_sections_values;
+            }
+            $data_sections_values = substr($data_sections_values, 0, -1);
+        }
+
+        $Permissions = new Permissions;
+        $Permissions->name = $request->name;
+        $Permissions->view_status = ($request->view_status) ? "1" : "0";
+        $Permissions->add_status = ($request->add_status) ? "1" : "0";
+        $Permissions->edit_status = ($request->edit_status) ? "1" : "0";
+        $Permissions->delete_status = ($request->delete_status) ? "1" : "0";
+        $Permissions->analytics_status = ($request->analytics_status) ? "1" : "0";
+        $Permissions->inbox_status = ($request->inbox_status) ? "1" : "0";
+        $Permissions->newsletter_status = ($request->newsletter_status) ? "1" : "0";
+        $Permissions->calendar_status = ($request->calendar_status) ? "1" : "0";
+        $Permissions->banners_status = ($request->banners_status) ? "1" : "0";
+        $Permissions->settings_status = ($request->settings_status) ? "1" : "0";
+        $Permissions->webmaster_status = ($request->webmaster_status) ? "1" : "0";
+        $Permissions->data_sections = $data_sections_values;
+        $Permissions->status = true;
+        $Permissions->save();
+
+        return redirect()->action('UsersController@index')->with('doneMessage', trans('backLang.addDone'));
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function permissions_edit($id)
+    {
+        //
+        // General for all pages
+        $GeneralWebmasterSections = WebmasterSection::where('status', '=', '1')->orderby('row_no', 'asc')->get();
+        // General END
+
+        if (@Auth::user()->permissionsGroup->view_status) {
+            $Permissions = Permissions::where('created_by', '=', Auth::user()->id)->find($id);
+        } else {
+            $Permissions = Permissions::find($id);
+        }
+        if (count($Permissions) > 0) {
+            return view("backEnd.users.permissions.edit", compact("Permissions", "GeneralWebmasterSections"));
+        } else {
+            return redirect()->action('UsersController@index');
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function permissions_update(Request $request, $id)
+    {
+        //
+        $Permissions = Permissions::find($id);
+        if (count($Permissions) > 0) {
+
+
+            $this->validate($request, [
+                'name' => 'required'
+            ]);
+
+            $data_sections_values = "";
+            if (@$request->data_sections != "") {
+                foreach ($request->data_sections as $key => $val) {
+                    $data_sections_values = $val . "," . $data_sections_values;
+                }
+                $data_sections_values = substr($data_sections_values, 0, -1);
+            }
+
+            $Permissions->name = $request->name;
+            $Permissions->view_status = ($request->view_status) ? "1" : "0";
+            $Permissions->add_status = ($request->add_status) ? "1" : "0";
+            $Permissions->edit_status = ($request->edit_status) ? "1" : "0";
+            $Permissions->delete_status = ($request->delete_status) ? "1" : "0";
+            $Permissions->analytics_status = ($request->analytics_status) ? "1" : "0";
+            $Permissions->inbox_status = ($request->inbox_status) ? "1" : "0";
+            $Permissions->newsletter_status = ($request->newsletter_status) ? "1" : "0";
+            $Permissions->calendar_status = ($request->calendar_status) ? "1" : "0";
+            $Permissions->banners_status = ($request->banners_status) ? "1" : "0";
+            $Permissions->settings_status = ($request->settings_status) ? "1" : "0";
+            $Permissions->webmaster_status = ($request->webmaster_status) ? "1" : "0";
+            $Permissions->data_sections = $data_sections_values;
+            $Permissions->status = $request->status;
+            if ($id != 1) {
+                $Permissions->save();
+            }
+            return redirect()->action('UsersController@permissions_edit', $id)->with('doneMessage',
+                trans('backLang.saveDone'));
+        } else {
+            return redirect()->action('UsersController@index');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function permissions_destroy($id)
+    {
+        //
+        if (@Auth::user()->permissionsGroup->view_status) {
+            $Permissions = Permissions::where('created_by', '=', Auth::user()->id)->find($id);
+        } else {
+            $Permissions = Permissions::find($id);
+        }
+        if (count($Permissions) > 0 && $id != 1) {
+
+            $Permissions->delete();
+            return redirect()->action('UsersController@index')->with('doneMessage', trans('backLang.deleteDone'));
+        } else {
+            return redirect()->action('UsersController@index');
+        }
+    }
+
+
+}
