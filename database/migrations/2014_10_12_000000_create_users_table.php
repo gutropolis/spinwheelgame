89e029308296b1e78f2eb24ceaf1c3bc5ce17b9e
gutropolis <gutropolis@gmail.com>
2018-06-16 19:42:39 +0530

    add

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+			$table->string('last_name');
+			$table->string('address')->nullable();
+			$table->string('gender')->nullable();
+			$table->string('country')->nullable();
+			$table->string('dob')->nullable();
+			$table->string('phone')->nullable();
+			$table->string('post_code')->nullable();
             $table->string('email')->unique();
             $table->string('password');
             $table->string('photo')->nullable();
