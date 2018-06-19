71672c7694df922f8d4e75d0588d7491302aea37
gutropolis <gutropolis@gmail.com>
2018-06-19 13:45:28 +0530

    add

---
+++ database/migrations/2018_06_19_062507_prize_list.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PrizeList extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+      public function up()
+    {
+		  Schema::create('prizelist', function (Blueprint $table) {
+          $table->increments('id');
+            $table->string('prize_name');
+            $table->string('descritpion');
+			 $table->string('points');
+			 $table->string('created_by');
+			 $table->timestamps();
+		 });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
