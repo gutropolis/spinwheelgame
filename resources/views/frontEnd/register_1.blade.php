664aef1c24ad3f381a0a80ea0a2f47aa2c58c9e6
gutropolis <gutropolis@gmail.com>
2018-06-19 20:07:14 +0530

    add

--- resources/views/frontEnd/register_1.blade.php
+++ resources/views/frontEnd/register_1.blade.php
@@ -4,7 +4,7 @@
 	
 		<section class="joinus">
       <div class="container">
-	
+	  
 
 
         <div class="joinus-logo">
@@ -15,7 +15,42 @@
             <p class="title-joinus">JOIN NOW FOR FREE REWARDS</p>
             <div class="joinus-block-in">
 			<form  name="registerForm" id="registerForm" method="post" action="/storedata">
+			
 					 {{ csrf_field() }}
+					 @if ($errors->any())
+<div class="alert alert-danger alert-dismissable margin5">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  <strong>Error:</strong> Please check the form below for errors
+</div>
+@endif
+
+@if ($message = Session::get('success_reg'))
+<div class="alert alert-success alert-dismissable margin5">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <strong>Success:</strong> {{ $message }}
+</div>
+@endif
+
+@if ($message = Session::get('error_reg'))
+<div class="alert alert-danger alert-dismissable margin5">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <strong>Error:</strong> {{ $message }}
+</div>
+@endif
+
+@if ($message = Session::get('warning_reg'))
+<div class="alert alert-warning alert-dismissable margin5">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <strong>Warning:</strong> {{ $message }}
+</div>
+@endif
+
+@if ($message = Session::get('info_reg'))
+<div class="alert alert-info alert-dismissable margin5">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <strong>Info:</strong> {{ $message }}
+</div>
+@endif
               <div class="row joinus-block-elements">
                   <div class="col-md-12 col-lg-12 elements-in">
                     <div class="form-heading">MY DETAILS</div>
