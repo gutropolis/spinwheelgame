bc438616d327c300d5baba5a5ffa7150581110b4
gutropolis <gutropolis@gmail.com>
2018-06-19 20:07:05 +0530

    add

--- resources/views/frontEnd/includes/header.blade.php
+++ resources/views/frontEnd/includes/header.blade.php
@@ -20,40 +20,7 @@
                   LOGIN TO YOUR ACCOUNT
                 </div>
                   <!--contactus-form-->
-				  @if ($errors->any())
-<div class="alert alert-danger alert-dismissable margin5">
-  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-  <strong>Error:</strong> Please check the form below for errors
-</div>
-@endif
-
-@if ($message = Session::get('success'))
-<div class="alert alert-success alert-dismissable margin5">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <strong>Success:</strong> {{ $message }}
-</div>
-@endif
-
-@if ($message = Session::get('error'))
-<div class="alert alert-danger alert-dismissable margin5">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <strong>Error:</strong> {{ $message }}
-</div>
-@endif
-
-@if ($message = Session::get('warning'))
-<div class="alert alert-warning alert-dismissable margin5">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <strong>Warning:</strong> {{ $message }}
-</div>
-@endif
-
-@if ($message = Session::get('info'))
-<div class="alert alert-info alert-dismissable margin5">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <strong>Info:</strong> {{ $message }}
-</div>
-@endif
+				 
                   <div class="contactus-form">
                        <form name="loginform" id="loginform" method="post" action="/checklogin">
 					 {{ csrf_field() }}
