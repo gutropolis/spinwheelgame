efd839be52b736f6f21a4c85781aef028b239db1
gutropolis <gutropolis@gmail.com>
2018-06-22 17:49:11 +0530

    add

--- resources/views/frontEnd/subscribepopup.blade.php
+++ resources/views/frontEnd/subscribepopup.blade.php
@@ -9,7 +9,14 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h4 class="modal-title" id="myModalLabel">
-                        <img src="frontEnd/images/white_raodeals_logo.png" class="logo"">
+                        <a class="navbar-brand"  href="{{ route('Home') }} ">
+                    @if(Helper::GeneralSiteSettings("style_logo_" . trans('backLang.boxCode')) !="")
+                        <img alt="" src="{{ URL::to('uploads/settings/'.Helper::GeneralSiteSettings("style_logo_" . trans('backLang.boxCode'))) }}">
+                    @else
+                        <img alt="" src="{{ URL::to('uploads/settings/nologo.png') }}">
+                    @endif
+
+                </a>
                     </h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" onclick="closeNav()">&times;</span></button>
                     
@@ -21,15 +28,16 @@
                               <img src="frontEnd/images/sidebar-spinwin.png" class="spinwheel-img">
                           </div>
                           <div class="col-lg-6 col-md-8 col-sm-6 col-6 spinwheel-text">
-                            <form>
+                            <form name="subscribepopup" id="subscribepopup" method="post" action="/subscribepopup">
+							 {{ csrf_field() }}
                               <h3 class="heading-spinwheel">GET YOUR CHANCE TO <span class="blue-clr">WIN A PRIZE!</span></h3>
                               <P class="title-text">Enter your email below and spin the wheel to see if you're our next 
                                   lucky winner! Your email
                               </P>
                               <div class="form-group">
-                                  <input type="email" class="form-control mail-input"  required placeholder="Enter Your Email">
+                                  <input type="email" name="email" class="form-control mail-input"  required placeholder="Enter Your Email">
                               </div>
-                              <a href="{{route('signup')}}" type="button" class="lucky-btn ">Try Your Luck</a>
+                              <button type="submit" class="lucky-btn ">Try Your Luck</button>
                             </form>
                           </div>
                       </div>
