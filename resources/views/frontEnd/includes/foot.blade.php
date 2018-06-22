424415f476f3da8584fde89fe415519a13d0ad15
gutropolis <gutropolis@gmail.com>
2018-06-22 17:48:21 +0530

    add

--- resources/views/frontEnd/includes/foot.blade.php
+++ resources/views/frontEnd/includes/foot.blade.php
@@ -7,13 +7,15 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+	 
     <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.js"></script>
+	
     <script src="/FrontEnd/js/validation.js"></script>
 
     <!--for spinner  -->   
