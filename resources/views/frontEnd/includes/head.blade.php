3b63bf54e2e9887e6110ddf8087dfb7cd31a41a5
gutropolis <gutropolis@gmail.com>
2018-06-22 17:48:41 +0530

    add

--- resources/views/frontEnd/includes/head.blade.php
+++ resources/views/frontEnd/includes/head.blade.php
@@ -3,7 +3,7 @@
 <meta name="description" content=""/>
 <meta name="keywords" content=""/>
 <meta name="author" content="{{ URL::to('') }}"/>
-<meta name="csrf-token" content="{{ csrf_token() }}" />
+<meta name="csrf-token" content = "{{ csrf_token() }}" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link href="{{ URL::asset('frontEnd/css/bootstrap.min.css') }}" rel="stylesheet"/>
 <link href="{{ URL::asset('frontEnd/css/fancybox/jquery.fancybox.css') }}" rel="stylesheet">
@@ -27,3 +27,27 @@
  <!--custom css-->
 <link href="{{ URL::asset('frontEnd/fonts/stylesheet.css') }}" rel="stylesheet"/>
 <link rel="stylesheet" type="text/css" href="{{ URL::to('css/app.css')}}">
+
+
+
+<!-- Favicon and Touch Icons -->
+@if(Helper::GeneralSiteSettings("style_fav") !="")
+    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_fav")) }}" rel="shortcut icon"
+          type="image/png">
+@else
+    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="shortcut icon" type="image/png">
+@endif
+@if(Helper::GeneralSiteSettings("style_apple") !="")
+    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon">
+    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon"
+          sizes="72x72">
+    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon"
+          sizes="114x114">
+    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon"
+          sizes="144x144">
+@else
+    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon">
+    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon" sizes="72x72">
+    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon" sizes="114x114">
+    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon" sizes="144x144">
+@endif
