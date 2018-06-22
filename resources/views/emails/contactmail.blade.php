a617ba1e96a3c56bd69c002427daf217dc59bd9a
gutropolis <gutropolis@gmail.com>
2018-06-22 19:35:16 +0530

    add

--- resources/views/emails/contactmail.blade.php
+++ resources/views/emails/contactmail.blade.php
@@ -1,8 +1,37 @@
-# Hello
-We have received a new contact mail.<br />
+<div class="container">
+   <div class="row">
+      <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;">
+               <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">Hello!! We Have Recieved A Contact Mail</h1>
+            </div>
+            <p style="font-size:13px;"><strong>Full Name:-</strong> {{ $contact_name }} </p>
+			<p style="font-size:13px;"><strong>Phone Number:-</strong> {{ $contact_phone }} </p>
+			 <p style="font-size:13px;"><strong>Subject:-</strong> {{ $contact_subject }}</p>
+            <p style="font-size:13px;"><strong>Email ID:-</strong> {{ $contact_email }}</p>
+			<p style="font-size:13px;"><strong>Message:-</strong> {{ $contact_msg }}</p>
 
-**Name :{{ $name }}<br />
-**Email :{{ $contact_email}}<br />
+         </div>
+        
+      </div>
+   </div>
+</div>
+</div>
+</div>
 
-**Message :{{ $contact_msg }} 
-Thanks,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
